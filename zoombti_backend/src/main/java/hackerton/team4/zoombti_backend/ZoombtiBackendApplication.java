64c0d5d8b15b869b7f36400d3bfd33494f5e3557
MyeongQ <gmk0904@ajou.ac.kr>
2023-06-29 01:13:16 +0900

    [Feat] Create All Settings Files

---
+++ zoombti_backend/src/main/java/hackerton/team4/zoombti_backend/ZoombtiBackendApplication.java
@@ -0,0 +1,13 @@
+package hackerton.team4.zoombti_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoombtiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoombtiBackendApplication.class, args);
+	}
+
+}
---
+++ zoombti_backend/src/test/java/hackerton/team4/zoombti_backend/ZoombtiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerton.team4.zoombti_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoombtiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
