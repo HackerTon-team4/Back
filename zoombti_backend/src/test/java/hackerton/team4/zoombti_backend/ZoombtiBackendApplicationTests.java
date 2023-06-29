d7386ca16f18c1dae111602020a9453e8fd51da6
MyeongQ <gmk0904@ajou.ac.kr>
2023-06-29 12:56:16 +0900

    [Fix] springboot test 주석처리

--- zoombti_backend/src/test/java/hackerton/team4/zoombti_backend/ZoombtiBackendApplicationTests.java
+++ zoombti_backend/src/test/java/hackerton/team4/zoombti_backend/ZoombtiBackendApplicationTests.java
@@ -3,7 +3,7 @@ package hackerton.team4.zoombti_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ZoombtiBackendApplicationTests {
 
 	@Test
